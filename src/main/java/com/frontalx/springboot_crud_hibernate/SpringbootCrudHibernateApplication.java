4ac554f67341052d14838387e368636bc81b0a0e
Abhishek Bansal <abhishek.bansal12@gmail.com>
2025-04-13 16:09:04 +0530

    initial project commit

---
+++ src/main/java/com/frontalx/springboot_crud_hibernate/SpringbootCrudHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.frontalx.springboot_crud_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frontalx/springboot_crud_hibernate/SpringbootCrudHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontalx.springboot_crud_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
